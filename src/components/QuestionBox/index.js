430d59edf81c8eb5d8d2d6c459622c9cb51131c4
Seongju38 <ljg013@naver.com>
2025-03-19 15:46:28 +0900

    QuestionBox 컴포넌트 안에 존재하는 컴포넌트 (Title, Desc, Body, TextInput, ActionButtons) 생성

--- src/components/QuestionBox/index.js
+++ src/components/QuestionBox/index.js
@@ -1,5 +1,17 @@
+import ActionButtons from '../ActionButtons';
+import Body from '../Body';
+import Desc from '../Desc';
+import Title from '../Title';
+
 function QuestionBox() {
-  return <>Q</>;
+  return (
+    <div>
+      <Title>타이틀입니다.</Title>
+      <Desc>설명입니다.</Desc>
+      <Body type={'text'} />
+      <ActionButtons />
+    </div>
+  );
 }
 
 export default QuestionBox;
