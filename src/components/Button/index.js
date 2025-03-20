401bfed79c5bfc664e1f4a2d9a15a6c61dc7b88b
Seongju38 <ljg013@naver.com>
2025-03-20 17:34:35 +0900

    코드 축약

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,18 +2,8 @@ import styled from 'styled-components';
 
 import { PRIMARY, SECONDARY, TERTIARY } from '../../constants/color';
 
-// type2는 DEFAULT, HOVER, .... 관련된 변수
-// function getColor(type1, type2) {
-//   if (type1 === 'PRIMARY') {
-//     return PRIMARY.BUTTON.DEFAULT.COLOR;
-//   } else if (type1 === 'SECONDARY') {
-//     return SECONDARY.BUTTON.DEFAULT.COLOR;
-//   }
-//   return;
-// }
-
 const colorMap = {
-  PRIMARY, // === PRIMARY: PRIMARY
+  PRIMARY,
   SECONDARY,
   TERTIARY,
 };
@@ -21,22 +11,8 @@ const colorMap = {
 const Button = styled.button`
   padding: 16px 24px;
   width: 200px;
-  color: ${(props) => {
-    // 이렇게 되면 코드가 너무 복잡해짐
-    // if (props.type === 'PRIMARY') {
-    //   return PRIMARY.BUTTON.DEFAULT.COLOR;
-    // } else if (props.type === 'SECONDARY') {
-    //   return SECONDARY.BUTTON.DEFAULT.COLOR;
-    // }
-
-    // 간결한 코드를 위한
-    // 첫번째 방법 ) 함수
-    // return getColor(props.type);
-
-    // 두번째 방법 ) map으로 관리
-    return colorMap[props.type].DEFAULT.COLOR;
-  }};
-  background: ${PRIMARY.BUTTON.DEFAULT.BACKGROUND};
+  color: ${({ type }) => colorMap[type].BUTTON.DEFAULT.COLOR};
+  background: ${({ type }) => colorMap[type].BUTTON.DEFAULT.BACKGROUND};
   border-radius: 4px;
   border: none;
   font-weight: bold;
