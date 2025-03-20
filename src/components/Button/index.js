04a5c94b4c8721dc860ae9a81293a2368a7f772c
Seongju38 <ljg013@naver.com>
2025-03-20 17:13:47 +0900

    Javascript Object로 Style 변수 생성 후 부분 적용

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,13 +1,15 @@
 import styled from 'styled-components';
 
+import { PRIMARY, SECONDARY, TERTIARY } from '../../constants/color';
+
 const Button = styled.button`
   padding: 16px 24px;
   width: 200px;
-  color: #ffffff;
-  font-weight: bold;
-  background: #6542f1;
+  color: ${PRIMARY.BUTTON.DEFAULT.COLOR};
+  background: ${PRIMARY.BUTTON.DEFAULT.BACKGROUND};
   border-radius: 4px;
   border: none;
+  font-weight: bold;
 `;
 
 export default Button;
---
+++ src/constants/color.js
@@ -0,0 +1,24 @@
+export const PRIMARY = {
+  BUTTON: {
+    DEFAULT: {
+      COLOR: '#FFFFFF',
+      BACKGROUND: '#6542F1',
+    },
+    HOVER: {
+      COLOR: '#FFFFFF',
+      BACKGROUND: '#9982F4',
+    },
+    PRESSED: {
+      COLOR: '#FFFFFF',
+      BACKGROUND: '#4B31B0',
+    },
+    DISABLED: {
+      COLOR: '#D0CDCD',
+      BACKGROUND: '#EDEDED',
+    },
+  },
+};
+
+export const SECONDARY = {};
+
+export const TERTIARY = {};
