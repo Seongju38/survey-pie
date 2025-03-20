272da04a745051a0eeb35a43730acdc9447e324c
Seongju38 <ljg013@naver.com>
2025-03-20 16:57:33 +0900

    Button 컴포넌트 생성 및 styled-components 적용

---
+++ src/components/Button/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  padding: 16px 24px;
+  width: 200px;
+  color: #ffffff;
+  font-weight: bold;
+  background: #6542f1;
+  border-radius: 4px;
+  border: none;
+`;
+
+export default Button;
