54966cfd0f2964a6b590aa47198c4f0512880bdc
Seongju38 <ljg013@naver.com>
2025-03-20 17:39:15 +0900

    hover, active, disabled 시 버튼 스타일 다르게 적용

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -16,6 +16,22 @@ const Button = styled.button`
   border-radius: 4px;
   border: none;
   font-weight: bold;
+
+  // & 자기 자신을 가르키는 선택자 (SCSS 문법) - 여기선 button
+  &:hover {
+    color: ${({ type }) => colorMap[type].BUTTON.HOVER.COLOR};
+    background: ${({ type }) => colorMap[type].BUTTON.HOVER.BACKGROUND};
+  }
+
+  &:active {
+    color: ${({ type }) => colorMap[type].BUTTON.PRESSED.COLOR};
+    background: ${({ type }) => colorMap[type].BUTTON.PRESSED.BACKGROUND};
+  }
+
+  &:disabled {
+    color: ${({ type }) => colorMap[type].BUTTON.DISABLED.COLOR};
+    background: ${({ type }) => colorMap[type].BUTTON.DISABLED.BACKGROUND};
+  }
 `;
 
 export default Button;
