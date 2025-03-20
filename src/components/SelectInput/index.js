16852e0176e7490b78475bba3f090b8f2bcdbbf9
Seongju38 <ljg013@naver.com>
2025-03-20 10:21:56 +0900

    SelectInput 에 styled-components 적용
    기존 checkbox는 안 보이게만 할뿐 선택 유무를 알려주고 그에 따라 checkbox 디자인이 다르게 보이도록 구현

--- src/components/SelectInput/index.js
+++ src/components/SelectInput/index.js
@@ -2,23 +2,61 @@ import styled from 'styled-components';
 
 function Item({ children }) {
   return (
-    <div>
+    <ItemWrapper>
       <label>
         <input type="checkbox" />
-        {children}
+        <span />
+        <div>{children}</div>
       </label>
-    </div>
+    </ItemWrapper>
   );
 }
 
 function SelectInput({ answer, setAnswer, options }) {
   return (
-    <div>
+    <SelectInputWrapper>
       {options.items.map((item, index) => {
         return <Item key={index}>{item}</Item>;
       })}
-    </div>
+    </SelectInputWrapper>
   );
 }
+
+const SelectInputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+`;
+
+const ItemWrapper = styled.div`
+  input[type='checkbox'] {
+    display: none;
+  }
+
+  input[type='checkbox'] ~ span {
+    width: 24px;
+    height: 24px;
+    border: 3px solid #e2dfdf;
+    box-sizing: border-box;
+    display: inline-block;
+    border-radius: 100%;
+    vertical-align: middle;
+    margin-right: 10px;
+  }
+
+  input[type='checkbox']:checked ~ span {
+    border: 8px solid #6542f1;
+  }
+
+  input[type='checkbox'] ~ div {
+    font-size: 14px;
+    line-height: 18px;
+    display: inline-block;
+  }
+
+  input[type='checkbox']:checked ~ div {
+    font-weight: bold;
+  }
+`;
 
 export default SelectInput;
