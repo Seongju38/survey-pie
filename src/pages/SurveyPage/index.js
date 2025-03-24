95f8792985f12ec254938f06b5172436d8911d60
Seongju38 <ljg013@naver.com>
2025-03-24 15:52:36 +0900

    answers 데이터에 Recoil 적용 및 QuestionBox에 questions, answers atom 연결

--- src/pages/SurveyPage/index.js
+++ src/pages/SurveyPage/index.js
@@ -1,38 +1,13 @@
-import { useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { useRecoilValue } from 'recoil';
 import styled from 'styled-components';
 
 import ProgressIndicator from '../../components/ProgressIndicator';
 import QuestionBox from '../../components/QuestionBox';
-import questionsState from '../../stores/questions/atom';
 
 function SurveyPage() {
-  const params = useParams();
-  console.log(params);
-
-  const questions = useRecoilValue(questionsState);
-
-  const step = parseInt(params.step);
-
-  const [answers, setAnswers] = useState([]);
-
   return (
     <SurveyPageWrapper>
       <ProgressIndicator />
-      <QuestionBox
-        question={questions[step]}
-        questionsLength={questions.length}
-        step={step}
-        answer={answers[step]}
-        setAnswer={(newAnswer) => {
-          setAnswers((answers) => {
-            const newAnswers = [...answers];
-            newAnswers[step] = newAnswer;
-            return newAnswers;
-          });
-        }}
-      />
+      <QuestionBox />
     </SurveyPageWrapper>
   );
 }
