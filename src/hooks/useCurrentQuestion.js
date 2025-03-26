7df16a357323d4dbeda48c3b2c566d5d308bb3bf
Seongju38 <ljg013@naver.com>
2025-03-26 19:07:34 +0900

    survey 데이터 API 연동

--- src/hooks/useCurrentQuestion.js
+++ src/hooks/useCurrentQuestion.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+import { useEffect } from 'react';
 import { useRecoilValue } from 'recoil';
 
 import surveyState from '../stores/survey/atom';
@@ -6,6 +8,12 @@ import useStep from './useStep';
 function useCurrentQuestion() {
   const step = useStep();
   const questions = useRecoilValue(surveyState).questions;
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/surveys/1').then((res) => {
+      console.log(res);
+    });
+  }, []);
 
   return questions[step];
 }
--- src/stores/survey/atom.js
+++ src/stores/survey/atom.js
@@ -2,39 +2,7 @@ import { atom } from 'recoil';
 
 const surveyState = atom({
   key: 'surveyState',
-  default: {
-    id: 1,
-    title: '타이틀',
-    questions: [
-      {
-        title: '질문1 입니다.',
-        desc: '설명1 입니다.',
-        type: 'select',
-        required: false,
-        options: {
-          items: ['답변1', '답변2', '답변3', '답변4', '답변5'],
-        },
-      },
-      {
-        title: '질문2 입니다.',
-        desc: '설명2 입니다.',
-        type: 'text',
-        required: false,
-        options: {
-          placeholder: 'placeholder 입니다.',
-        },
-      },
-      {
-        title: '질문3 입니다.',
-        desc: '설명3 입니다.',
-        type: 'textarea',
-        required: false,
-        options: {
-          placeholder: 'placeholder 입니다.',
-        },
-      },
-    ],
-  },
+  default: null,
 });
 
 export default surveyState;
