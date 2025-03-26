08ce3cf9c08e78c1ce984ce4c64b3dfad630564a
Seongju38 <ljg013@naver.com>
2025-03-26 18:56:51 +0900

    API 에서 내려주는 survey 데이터와 임시 데이터 형태 맞추기

--- src/hooks/useCurrentQuestion.js
+++ src/hooks/useCurrentQuestion.js
@@ -1,11 +1,11 @@
 import { useRecoilValue } from 'recoil';
 
-import questionsState from '../stores/questions/atom';
+import surveyState from '../stores/survey/atom';
 import useStep from './useStep';
 
 function useCurrentQuestion() {
   const step = useStep();
-  const questions = useRecoilValue(questionsState);
+  const questions = useRecoilValue(surveyState).questions;
 
   return questions[step];
 }
