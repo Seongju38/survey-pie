5fea9ed22ac1cc04d390ea2c895cb15da83a97fb
Seongju38 <ljg013@naver.com>
2025-03-28 16:47:39 +0900

    useCurrentQuestion 코드 정리 및 누락된 api 코드 정리

--- src/stores/survey/surveyState.js
+++ src/stores/survey/surveyState.js
@@ -1,13 +1,11 @@
 import { selector } from 'recoil';
 
-import mainApi from '../../services/apis/mainApi';
+import getSurvey from '../../services/getSurvey';
 
 const surveyState = selector({
   key: 'surveyState',
   get: async ({ get }) => {
-    const res = await mainApi.get(
-      `/surveys/${window.location.pathname.split('/')[2]}`,
-    );
+    const res = await getSurvey(window.location.pathname.split('/')[2]);
 
     return res.data;
   },
