f71b4cbb45f5576bc36781197e259c77dcfe0b82
Seongju38 <ljg013@naver.com>
2025-04-16 20:23:59 +0900

    ProgressIndicator 에서 질문의 개수 및 답변 상태에 따라 Bar 렌더링

--- src/components/ProgressIndicator/index.js
+++ src/components/ProgressIndicator/index.js
@@ -1,15 +1,33 @@
+import { useRecoilValue } from 'recoil';
 import styled from 'styled-components';
 
+import useAnswers from '../../hooks/useAnswers';
+import useStep from '../../hooks/useStep';
+import questionsLengthState from '../../stores/survey/questionsLengthState';
 import Bar from '../Bar';
 
 function ProgressIndicator() {
+  const length = useRecoilValue(questionsLengthState);
+  const answers = useAnswers();
+  const step = useStep();
+
+  const bars = [];
+  for (let i = 0; i < length; i++) {
+    let status = 'pending';
+    if (i === step) {
+      status = 'in-progress';
+    } else if (answers[i]) {
+      status = 'done';
+    }
+
+    bars.push(<Bar key={i} status={status} />);
+  }
+
   return (
     <ProgressIndicatorWrapper>
-      <Bar status="done" />
-      <Bar status="in-progress" />
-      <Bar status="pending" />
+      {bars}
       <PageCount>
-        <span>2</span>/3
+        <span>{step + 1}</span>/{length}
       </PageCount>
     </ProgressIndicatorWrapper>
   );
