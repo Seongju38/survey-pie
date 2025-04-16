0983a986a8ab4b04d86ae1a7b49d667424a2f8b8
Seongju38 <ljg013@naver.com>
2025-04-16 19:41:56 +0900

    ProgressIndicator 기본 구조 및 Bar 컴포넌트 생성

---
+++ src/components/Bar/index.js
@@ -0,0 +1,12 @@
+// status = 'pending' | 'in-progress' | 'done'
+
+import styled from 'styled-components';
+
+const Bar = styled.div`
+  height: 8px;
+  width: 120px;
+  border-radius: 16px;
+  background-color: aqua;
+`;
+
+export default Bar;
--- src/components/ProgressIndicator/index.js
+++ src/components/ProgressIndicator/index.js
@@ -1,5 +1,18 @@
+import styled from 'styled-components';
+
+import Bar from '../Bar';
+
 function ProgressIndicator() {
-  return <></>;
+  return (
+    <ProgressIndicatorWrapper>
+      <Bar />
+      <Bar />
+      <Bar />
+      <div>2/3</div>
+    </ProgressIndicatorWrapper>
+  );
 }
+
+const ProgressIndicatorWrapper = styled.div``;
 
 export default ProgressIndicator;
