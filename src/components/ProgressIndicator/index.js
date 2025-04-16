36b977c0412de6e4faeea3c2c9bf1ed163c91bba
Seongju38 <ljg013@naver.com>
2025-04-16 19:53:26 +0900

    ProgressIndicator 위치 조정

--- src/components/ProgressIndicator/index.js
+++ src/components/ProgressIndicator/index.js
@@ -8,11 +8,24 @@ function ProgressIndicator() {
       <Bar />
       <Bar />
       <Bar />
-      <div>2/3</div>
+      <PageCount>2/3</PageCount>
     </ProgressIndicatorWrapper>
   );
 }
 
-const ProgressIndicatorWrapper = styled.div``;
+const ProgressIndicatorWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  position: absolute;
+  bottom: calc(100% + 24px);
+  left: 0;
+  width: 100%;
+  align-items: center;
+  gap: 8px;
+`;
+
+const PageCount = styled.div`
+  margin-left: 8px;
+`;
 
 export default ProgressIndicator;
